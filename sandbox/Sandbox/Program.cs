fb98e609e5564c32d8a5c293e04d84d9341c7ae3
franco-ermacora <ermacora.fran@gmail.com>
2023-01-07 22:47:05 -0300

    add lines

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,8 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Sandbox World!");
+        Console.WriteLine("This is C#");
+        int number = 5;
+        Console.WriteLine(number);
     }
 }
